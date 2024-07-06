a74f303b15c3f5b562dbdf90af1b3ef8307ebdd9
mo.mousavi <s.m.mousavi1993@gmail.com>
2024-07-06 14:04:30 +0330

    fix: people and person queries modifed as the document requairements.

--- core/data/datasource/src/main/java/com/github/smmousavi/datasource/film/remote/DefaultFilmRemoteDataSource.kt
+++ core/data/datasource/src/main/java/com/github/smmousavi/datasource/film/remote/DefaultFilmRemoteDataSource.kt
@@ -1,13 +1,8 @@
 package com.github.smmousavi.datasource.film.remote
 
 import com.apollographql.apollo3.ApolloClient
-import com.apollographql.apollo3.api.ApolloResponse
-import com.github.smmousavi.network.PeopleQuery
-import com.github.smmousavi.network.PersonQuery
 import javax.inject.Inject
 
 class DefaultFilmRemoteDataSource @Inject constructor(private val apolloClient: ApolloClient) :
     FilmRemoteDataSource {
-
-
 }
