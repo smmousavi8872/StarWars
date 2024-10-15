8d50409eb99aa49eb3971c5e31a2ec01c9428947
Mohsen Mousavi <s.m.mousavi1993@gmail.com>
2024-10-15 03:42:49 +0330

    fix(repository): error message sent through the ApolloException

--- core/data/repository/src/main/java/com/github/smmousavi/repository/films/DefaultFilmsRepository.kt
+++ core/data/repository/src/main/java/com/github/smmousavi/repository/films/DefaultFilmsRepository.kt
@@ -1,5 +1,6 @@
 package com.github.smmousavi.repository.films
 
+import com.apollographql.apollo3.exception.ApolloException
 import com.github.smmousavi.common.result.Result
 import com.github.smmousavi.datasource.film.remote.FilmRemoteDataSource
 import kotlinx.coroutines.flow.flow
@@ -8,18 +9,16 @@ import javax.inject.Inject
 class DefaultFilmsRepository @Inject constructor(
     private val remoteDataSource: FilmRemoteDataSource,
 ) : FilmsRepository {
-
     override suspend fun subscribeOnNewFilms() = flow {
         emit(Result.Loading)
         remoteDataSource.onNewFilmsSubscription().collect { response ->
             if (response.hasErrors()) {
                 response.errors?.forEach { error ->
-                    emit(Result.Error(IllegalArgumentException()))
+                    emit(Result.Error(ApolloException(error.message)))
                 }
             } else {
                 emit(Result.Success(response.data?.allFilms?.films))
             }
         }
     }
-
 }
